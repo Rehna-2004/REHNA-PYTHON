8b499a5810d656df6435a3931009c060b8462cc2
Rehna-2004 <rehnampta@gmail.com>
2025-10-16 05:36:21 -0400

    Add files via upload

---
+++ co4.py
@@ -0,0 +1,30 @@
+class bankaccount():
+    def __init__(self,accno,accname,acctype,accbala):
+        self.accno=accno
+        self.accname=accname
+        self.acctype=acctype
+        self.accbala=accbala
+    def deposit(self,amount):
+        self.accbala+=amount
+        print("current balance=", self.accbala)
+        print("amount deposited:",amount)
+    def accountinfo(self):
+        print("Account Number=",self.accno)
+        print("Account Name=",self.accname)
+        print("Account Type=",self.acctype)
+        print("Account Balance=",self.accbala)
+    def withdraw(self,amount):
+        if amount>self.accbala:
+          print("Insufficient balance!")
+        else:
+           self.accbala -= amount 
+           print("withdrawn:",amount, "remaining balance",self.accbala)
+
+no=int(input("Enter the account number:"))
+name=input("Enter the account name:")
+acctype=input("Enter the account type:")
+bal=float(input("Enter the account balance:"))
+obj=bankaccount(no,name,acctype,bal)
+depo=int(input("Enter the amount to be deposited:"))
+obj.deposit(depo)
+
