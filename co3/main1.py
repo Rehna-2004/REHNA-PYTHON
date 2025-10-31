111ded94a0c2b41d1104aaf90ca76a9a6125d8eb
Rehna-2004 <rehnampta@gmail.com>
2025-10-31 12:49:09 +0530

    Add files via upload

---
+++ co3/main1.py
@@ -0,0 +1,12 @@
+from math1 import arithmetic, rectangle
+from math1.advance import triangle
+
+print("Rectangle area:", rectangle.area(10, 5))
+print("Rectangle perimeter:", rectangle.perimeter(10, 5))
+
+print("Addition:", arithmetic.add(20, 5))
+print("subtraction:", arithmetic.sub(20, 5))
+
+print("Triangle_area:", triangle.tri_area(5, 4))
+
+
