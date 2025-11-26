3233620eda2e4baebfe669c9fd367f912e655d1a
Rehna-2004 <rehnampta@gmail.com>
2025-11-26 14:23:16 +0530

    Add files via upload

---
+++ co1_10.py
@@ -0,0 +1,14 @@
+import math
+radius=float(input("Enter the radius of a circle:"))
+area=math.pi*radius*radius
+print("Area of circle is",area)
+
+length=float(input("Enter the length of rectangle:"))
+width=float(input("Enter the width of rectangle:"))
+area=(length*width)
+print("Area of rectangle is",area)
+
+b=float(input("Enter the base of triangle:"))
+h=float(input("enter the height of triangle:"))
+area=b*h/2
+print("Area of Triangle:",area)
