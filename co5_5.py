1ce21d9fa63e9c77369f97279a00c712c5c92589
Rehna-2004 <rehnampta@gmail.com>
2025-11-27 14:09:51 +0530

    Add files via upload

---
+++ co5_5.py
@@ -0,0 +1,25 @@
+import csv
+#input dictionary using user input method
+data={}
+n=int(input("Enter the limit"))
+for i in range(n):
+    key=input("Enter the key")
+    val=input("Enter the value")
+    data[key]=val
+print(data)
+
+#csv writing
+f=open("dictdata.csv","w",newline='')
+content=csv.writer(f)
+content.writerow(data.keys())
+content.writerow(data.values())
+f.close()
+
+#csv reading
+f1=open("dictdata.csv","r")
+cont=csv.reader(f1)
+for i in cont:
+    print(i)
+f1.close()    
+
+      
